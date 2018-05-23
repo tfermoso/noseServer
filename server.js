f1a55f4ea337db4997bd74aabecdf12c687e08ab
tfermoso <tomas.fermoso@gmail.com>
2018-05-23 13:54:30 +0200

    app tareas funcionando contra bbdd mysql

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 var fs = require('fs');
-var listaTareas = [];
-
-fs.exists("tareas.json", function (encontrado) {
-
-  if (encontrado) {
-    console.log("cargando datos...");
-    var data = fs.readFileSync("tareas.json", "UTF-8")
-    listaTareas = JSON.parse(data);
-  } else {
-    listaTareas = [];
-  }
-});
-
-
 
 var app = express();
-
 // create application/json parser
 var jsonParser = bodyParser.json()
 // create application/x-www-form-urlencoded parser
@@ -26,89 +12,107 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(jsonParser);
 app.use(urlencodedParser);
 
-app.use(express.static('www/Tareas'));
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'master',
+  password: 'bvIRWz1PD2Ajyv9J',
+  database: 'tareasDB'
+});
+connection.connect(function (error) {
+  if (error) {
+    throw error;
+  } else {
+    console.log('Conexion correcta con el servidor.');
+  }
+});
+
 
 app.post('/', function (req, res) {
   console.log("petición recibida");
   var nomb = req.body.nombre || '';
   var tar = req.body.tarea || '';
   var nuevaTarea = { nombre: nomb, tarea: tar };
-  listaTareas.push(nuevaTarea);
-  actualizarBBDD();
-  //console.log(listaTareas);
-  res.redirect('/');
-  /*
-  fs.readFile('./www/Tareas/index2.html', 'utf8', function (err, text) {
-    var fila = cargarTareas(listaTareas);
 
-    text = text.replace("[sustituir]", fila);
-    res.send(text);
-  });
-  //res.send('hello '+nombre);
-  */
+  connection.query('INSERT INTO tareas (nombre, tarea) VALUES(?, ?)', [nomb, tar], function (error, result) {
+    if (error) {
+      throw error;
+    } else {
+      console.log(result);
+      res.redirect('/');
+    }
+  }
+  );
 });
 
 app.get('/', function (req, res) {
   console.log("petición recibida en tareas");
+  connection.query("select * from tareas", function (err, result) {
+    fs.readFile('./www/Tareas/index.html', 'utf8', function (err, text) {
 
+      var fila = cargarTareas(result)
 
-  fs.readFile('./www/Tareas/index2.html', 'utf8', function (err, text) {
+      text = text.replace("[sustituir]", fila);
+      res.send(text);
+    });
+  })
 
-    var fila = cargarTareas(listaTareas);
-
-    text = text.replace("[sustituir]", fila);
-    res.send(text);
-  });
   //res.send('hello '+nombre);
 });
 
 
 app.get('/eliminar/:id?', function (req, res) {
   console.log("Eliminando registro " + req.query.id);
-  listaTareas.splice(req.query.id, 1);
-  //Eliminar registro de la colección;
-
-  actualizarBBDD();
-  res.redirect('/');
-  /*
-    fs.readFile('./www/Tareas/index2.html', 'utf8', function (err, text) {
-      var fila = cargarTareas(listaTareas);
-  
-      text = text.replace("[sustituir]", fila);
-      
-      res.send(text);
-    });
-    */
-});
-
-app.get('/editar/:id?',function(req,res){
-
-  fs.readFile('./www/Tareas/index2.html', 'utf8', function (err, text) {
-    var fila = cargarTareas(listaTareas);
-    var nombre=listaTareas[req.query.id].nombre;
-    var tarea=listaTareas[req.query.id].tarea;
-    text = text.replace("[sustituir]", fila);
-    text = text.replace('action="/"','action="/editar"' );
-    text = text.replace("[id_editar]", req.query.id);
-    text=text.replace('placeholder="Nombre de usuario"','value="'+nombre+'"');
-    text=text.replace('placeholder="nombre de la tarea"','value="'+tarea+'"');
-    res.send(text);
+  connection.query("DELETE from tareas WHERE id = ?", [req.query.id], function (err, result) {
+    console.log("Record Deleted!!");
+    //console.log(result);
+    res.redirect('/');
   });
 });
 
-app.post('/editar',function(req,res){
-  
-  var nomb = req.body.nombre || '';
-  var tar = req.body.tarea || '';
-  var id=req.body.id;
-  listaTareas[id].nombre=nomb;
-  listaTareas[id].tarea=tar;
-  actualizarBBDD();
-  res.redirect('/');
+app.get('/editar/:id?', function (req, res) {
+  connection.query("Select * from tareas", function (err, result) {
+    var registroEditar;
+    console.log(result);
+    for (const tarea of result) {
+      if (tarea.id == req.query.id) {
+        registroEditar = tarea;
+      }
+    }
+    fs.readFile('./www/Tareas/index.html', 'utf8', function (err, text) {
+      var fila = cargarTareas(result);
+      var nombre = registroEditar.nombre;
+      var tarea = registroEditar.tarea;
+      text = text.replace("[sustituir]", fila);
+      text = text.replace('action="/"', 'action="/editar"');
+      text = text.replace("[id_editar]", req.query.id);
+      text = text.replace('placeholder="Nombre de usuario"', 'value="' + nombre + '"');
+      text = text.replace('placeholder="Nombre de la tarea"', 'value="' + tarea + '"');
+      res.send(text);
+    });
 
+  });
 
 });
 
+app.post('/editar', function (req, res) {
+
+  var nomb = req.body.nombre || '';
+  var tar = req.body.tarea || '';
+  var id = req.body.id;
+
+  connection.query("UPDATE tareas SET nombre=?, tarea=? WHERE id = ?", [nomb, tar, id], function (error, resultado) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(resultado);
+      res.redirect('/');
+    }
+  })
+});
+
+
+
+app.use(express.static('www/Tareas'));
 
 
 var server = app.listen(8080, function () {
@@ -116,7 +120,9 @@ var server = app.listen(8080, function () {
 });
 
 
+
 function cargarTareas(tareas) {
+
   var lista = "";
   for (var indice in tareas) {
     var fila = `
@@ -130,9 +136,9 @@ function cargarTareas(tareas) {
           </td>
       </tr>
       `;
-    
-    fila = fila.split("[id]").join(indice);
-    
+
+    fila = fila.split("[id]").join(tareas[indice].id);
+
     fila = fila.replace("[nombre]", tareas[indice].nombre);
     fila = fila.replace("[tarea]", tareas[indice].tarea);
     lista += fila;
@@ -140,8 +146,4 @@ function cargarTareas(tareas) {
   return lista;
 }
 
-function actualizarBBDD(){
-  fs.writeFile("tareas.json", JSON.stringify(listaTareas), function () {
-    console.log("Fichero de datos actualizado");
-  });
-}
+
--- www/Tareas/js/tareas.js
+++ www/Tareas/js/tareas.js
@@ -1,38 +1,7 @@
 window.onload = function () {
+    console.log(window.innerHeight-30);
+    var valor="height: "+(window.innerHeight-250)+"px;";
+    console.log(valor);
+    document.getElementById("tabla").setAttribute("style",valor);
 
-    var tareas = [
-        
-    ];
-
-    function cargarTareas(tareas) {
-        document.getElementById("tabla").innerHTML="";
-        for (var indice in tareas) {
-            var fila = `
-            <tr>
-                <td>[id]</td>
-                <td>[nombre]</td>
-                <td>[tarea]</td>
-            </tr>
-            `;
-            fila = fila.replace("[id]", indice);
-            fila = fila.replace("[nombre]", tareas[indice].nombre);
-            fila = fila.replace("[tarea]", tareas[indice].tarea);
-            document.getElementById("tabla").innerHTML += fila;
-        }
-    }
-
-    cargarTareas(tareas);
-
-    this.document.getElementById("enviar").onclick = function (event) {
-        //event.preventDefault();
-        var nomb = document.getElementById("nombre").value;
-        var tar = document.getElementById("tarea").value;
-        tareas.push({nombre:nomb,tarea:tar});
-        cargarTareas(tareas);
-
-    }
-
-    // this.document.getElementById("formulario").onsubmit=function(){
-    //     alert("formulario");
-    // }
 }
